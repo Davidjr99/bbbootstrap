963ea8315180fbc717784325a7c56087242ba190
David Junior <davidjunior3010@gmail.com>
2024-03-13 15:17:52 -0300

    rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursosModule } from './cursos/cursos.module';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {
+    path: '', pathMatch: 'full', redirectTo: 'cursos'
+  },
+
+  {
+    path:  'cursos',
+    loadChildren: () => CursosModule
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+
+}
